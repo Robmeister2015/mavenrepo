292335bf5ea9ddbbba0a5c621a6dde95271b6644
Robmeister2015 <A00226084@student.ait.ie>
2016-05-10 14:00:19 +0100

    message

---
+++ robart/src/main/java/rob/robart/GetSet.java
@@ -0,0 +1,10 @@
+package rob.robart;
+
+public class GetSet {
+
+	int thisInt = 0;
+	
+	public void setThisInt(int setMe){
+		thisInt = setMe;
+	}
+}
---
+++ robart/src/test/java/rob/robart/ThisIntTest.java
@@ -0,0 +1,15 @@
+package rob.robart;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class ThisIntTest extends TestCase {
+
+	@Test
+	public void testSetThisInt(){
+		GetSet g = new GetSet();
+		g.setThisInt(20);
+		assertEquals(20, g.thisInt);
+	}
+}
